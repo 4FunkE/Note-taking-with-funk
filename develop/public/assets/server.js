188b82175648044c0025ed7503cb099e797ec911
4FunkE <4FunkEcodes@gmail.com>
2023-07-14 16:42:48 -0400

    imported needed modules and files

---
+++ develop/public/assets/server.js
@@ -0,0 +1,4 @@
+const express = require("express");//import express
+const fs = require("fs");//import fs to read and write files
+const { v4: uuid4 } = require("uuid");//importing the v4 property from uuid named it uuid4
+const path = require("path");//import path to create file paths
